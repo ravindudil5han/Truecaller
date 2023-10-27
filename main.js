307a92e9d802c86ae807c96630e910fa2988319c
Ravindu Dilshan <90592440+ravindudil5han@users.noreply.github.com>
2023-10-27 16:39:41 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,22 @@
+const truecallerjs = require('truecallerjs');
+const express = require('express');
+const app = express();
+
+app.get('/search', (req, res) => {
+  var searchData = {
+      number: req.query.number,
+      countryCode: req.query.countryCode,
+      installationId: "your id",
+      output: "JSON"
+  }
+
+  var sn = truecallerjs.searchNumber(searchData);
+  sn.then(function(response) {
+    res.send(response);
+  });
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
